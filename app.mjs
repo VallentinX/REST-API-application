c8888b56799a430290f28e1d68f9e6aa6f81b5de
VallentinX <valentin.cristian_98@icloud.com>
2024-06-07 15:36:05 +0300

    Done

--- app.mjs
+++ app.mjs
@@ -1,7 +1,9 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-const contactsRouter = require('./routes/api/contacts');
+import express from 'express';
+import logger from 'morgan';
+import cors from 'cors';
+import contactsRouter from './routes/api/contacts.mjs';
+import usersRouter from './routes/api/users.mjs';
+
 const app = express();
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
@@ -10,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/contacts', contactsRouter);
+app.use('/api/users', usersRouter);
 
 app.use((req, res) => {
   res.status(404).json({message: 'Not found'});
@@ -19,4 +22,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({message: err.message});
 });
 
-module.exports = app;
+export default app;
--- server.mjs
+++ server.mjs
@@ -1,5 +1,9 @@
-const app = require('./app');
-const connectDB = require('./db');
+import app from './app.mjs';
+import connectDB from './db.mjs';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const port = process.env.PORT || 8000;
 
 connectDB().then(() => {
